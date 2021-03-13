d5b7892b24f3ae664e60ced7a1766f1bd6a829ed
MarinF00 <marinchoooooo@gmail.com>
2021-03-13 16:29:59 +0200

    Fixing security and page

--- Projects/FootballWebSite/src/main/java/com/self/FootballWebSite/controller/ClubController.java
+++ Projects/FootballWebSite/src/main/java/com/self/FootballWebSite/controller/ClubController.java
@@ -2,21 +2,22 @@ package com.self.FootballWebSite.controller;
 
 import com.self.FootballWebSite.bootstrap.BootStrapData;
 import com.self.FootballWebSite.repositories.ClubRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class ClubController {
 
-    final ClubRepository clubRepository;
+    public ClubRepository clubRepository;
 
     public ClubController(ClubRepository clubRepository)
     {
         this.clubRepository = clubRepository;
     }
-@RequestMapping("/Clubs")
+@RequestMapping("/MyClubs")
     public String getClubs(Model model)
     {
-    model.addAttribute("clubs", BootStrapData.clubs);
-    return "clubs";
+    model.addAttribute("clubs", clubRepository.findAll());
+    return "MyClubs";
     }
 }
