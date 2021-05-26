c3e20846fcb1a31902ad153e76d625993db8598e
MarinF00 <marinchoooooo@gmail.com>
2021-05-26 13:11:28 +0300

    MentorMate task

--- Projects/FootballWebSite/src/main/java/com/self/FootballWebSite/repositories/PublisherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.self.FootballWebSite.repositories;
-
-import com.self.FootballWebSite.model.Publisher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PublisherRepository extends CrudRepository<Publisher,Long> {
-}
---
+++ Projects/MentorMate/src/com/company/MyFile.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.io.*;
+
+public class ObjectIOExample {
+
+
+    public void WriteEmployeeToFile(String filepath, Employee employee)
+    {
+        try {
+            File file = new File(filepath);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter(filepath,true);
+            myWriter.write(employee.toString());
+            myWriter.flush();
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+
+    public void WriteReportToFile( String filepath, Report report) throws IOException {
+        try {
+            File file = new File(filepath);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter(filepath,true);
+            myWriter.write(report.toString());
+            myWriter.flush();
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
