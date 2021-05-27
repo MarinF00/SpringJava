2f7c9112cdb2c976b6b4bcf8cc07d56a3e76cccd
MarinF00 <marinchoooooo@gmail.com>
2021-05-27 18:29:23 +0300

    reading from json

---
+++ Projects/MentorMate/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        List<List<String>> myEmployees = new ArrayList<>();
+
+        String filepath1 = sc.nextLine();
+
+        MyFile myFile = new MyFile();
+
+
+
+        Employee Ivan = new Employee("Ivan",250,10,0.5);
+        Employee Gosho = new Employee("Gosho",250,10,0.5);
+        Report report = new Report(10,false,10);
+        Report report1 = new Report(10,true,10);
+
+    myFile.ReadFromFile(filepath1);
+
+
+    }
+
+
+}
